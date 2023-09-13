a2997e2fe3f2731c9eab587426a328dabdeaedb6
Vaiizz <102771864+Vaiizz@users.noreply.github.com>
2023-09-12 23:48:45 -0300

    exercicio 4

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -5,3 +5,7 @@ SELECT nome FROM autores WHERE nascimento < '1900-01-01';
 SELECT titulo FROM livros
 JOIN autores ON livros.autor_id = autores.id
 WHERE autores.nome = 'J.K. Rowling';
+
+SELECT alunos.nome FROM alunos
+JOIN matriculas ON alunos.id = matriculas.aluno_id
+WHERE matriculas.curso = 'Engenharia de Software';
