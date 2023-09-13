81a8c861b97f61745f8a163198d08dba1234010d
Vaiizz <102771864+Vaiizz@users.noreply.github.com>
2023-09-12 23:48:15 -0300

    exercicio 3

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -1,3 +1,7 @@
 SELECT titulo FROM livros;
 
 SELECT nome FROM autores WHERE nascimento < '1900-01-01';
+
+SELECT titulo FROM livros
+JOIN autores ON livros.autor_id = autores.id
+WHERE autores.nome = 'J.K. Rowling';
