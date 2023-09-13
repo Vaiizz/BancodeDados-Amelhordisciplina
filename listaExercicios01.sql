d4a209e8f0272b360dcd08c00220ec67bb5cd7a2
Vaiizz <102771864+Vaiizz@users.noreply.github.com>
2023-09-12 23:49:08 -0300

    exercicio 5

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -9,3 +9,7 @@ WHERE autores.nome = 'J.K. Rowling';
 SELECT alunos.nome FROM alunos
 JOIN matriculas ON alunos.id = matriculas.aluno_id
 WHERE matriculas.curso = 'Engenharia de Software';
+
+SELECT produto, SUM(receita) AS receita_total
+FROM vendas
+GROUP BY produto;
